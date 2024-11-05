10b2cc335979835f8a9dee1ec229c62c6b57b9e4
tarikcolakel <tarikcolakel@gmail.com>
2024-11-05 18:22:04 +0300

    yazılı kısım oluşturuldu

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,38 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import workintech from '/workintech.svg'
 import './App.css'
+import { NavLink, Label, Form, Input, FormGroup,Button } from 'reactstrap';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://github.com/Workintech/fsweb-s7-challenge-pizza" target="_blank">
-          <img src={workintech} className="logo" alt="Workintech logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Workintech + 🍕</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Absolute Acı Pizza sayısı {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Workintech or Pizza logos to learn more
-      </p>
-    </>
+    
+    <div className='App'>
+       <header>
+        <div className='header-container'>
+          <div className='header'><img className="logo" src="../Assets/Iteration-1-assets/logo.svg" alt="" />
+          <p>Anasayfa-Seçenekler - <strong>Sipariş Oluştur</strong> </p></div>
+          
+        </div>
+       </header>
+       <div className='p'>
+        <h2>Position Absolute Acı Pizza</h2>
+        <div className='fiyat'>
+          <p>85.50TL</p>
+          
+            <p>4.9 (200)</p>
+            
+          
+        </div>
+        <p>Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. . Küçük bir pizzaya bazen pizzetta denir.</p>
+        
+       </div>
+       
+       
+       </div>
+      
+      
   )
 }
 
