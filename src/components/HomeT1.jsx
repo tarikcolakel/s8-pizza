766bd67d4fa712ba07b2ccba1a9ab07b743f4b92
tarikcolakel <tarikcolakel@gmail.com>
2024-11-09 00:21:02 +0300

    commit düzeltildi

--- src/components/HomeT1.jsx
+++ src/components/HomeT1.jsx
@@ -1,9 +1,6 @@
 import { Form, FormGroup, Label, Input, ButtonGroup, Button } from 'reactstrap';
 import React from 'react';
-
-
 export default function Home() {
-
     return (
         <>
           <div className="backgroundİmg just-item-center padding-m ">
@@ -11,11 +8,7 @@ export default function Home() {
               <img className="flex center tekno-img" src="/public/logo.svg"/>
             </div>
             <div className="flex column center roboto beyaz font-45 max-width-s">
-<<<<<<< HEAD
-              <span className='center'>KOD ACIKTIRIR </span>
-=======
               <span className='center'>KOD  </span>
->>>>>>> 327c5478dbb2e8adc6db3ba6d21e11fe948d2915
               <span className='center'>PİZZA, DOYURUR</span>
             </div>
             <button className="sariBack no-border buton-kenar font-12">ACIKTIM</button>
